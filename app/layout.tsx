8293c9213bb574e906accb39978a8b562439d018
Igor <igos2006@gmail.com>
2023-08-04 22:47:56 +1000

    first-visible

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,18 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<head />
+			<head>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin=''
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700;800;900&display=swap'
+					rel='stylesheet'
+				/>
+			</head>
 			<body className={myFont.className}>
 				<MainProvider>
 					<div className='min-h-screen flex flex-col'>
